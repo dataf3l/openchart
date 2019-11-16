9e0a53681f2c1aa1f4e8a9da40bb49f421857027
anujkumarthakur <anuj85500@gmail.com>
2019-11-16 23:36:31 +0530

    add showPropertyEditor function return node object values

--- ui/property_editor.js
+++ ui/property_editor.js
@@ -1,4 +1,3 @@
-
 function propertyChanged(nodeId,propertyName,newValue){
 
 }
@@ -10,5 +9,18 @@ function propertyChanged(nodeId,propertyName,newValue){
 // for each property, show a text box
 // when the textboxcontent changes, call: propertyChanged()
 function showPropertyEditor(targetDivId, nodeId, node) {
-	
+	if(node.length > 0){
+		return (
+			"<div>"+"Name:" +"<input type='text'>" + node.type +"<br>"+
+		    	"x:" + "<input type='text'>" + node.x + "<br>"+
+		    	"y:" + "<input type='text'>" + node.y + "<br>"+
+		    	"width:" + "<input type='text'>" + node.width + "<br>"+ 
+		    	"height:" + "<input type='text'>" + node.height + "<br>"+ 
+		    	"name:" + "<input type="text">" + node.name + "<br>"+ 
+		    	"id:" + "<input type="text">" + node.id + "<br>"+ 
+			"</div>"
+		)
+	} else {
+		return false;
+	}
 }
