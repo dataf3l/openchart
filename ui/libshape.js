42a89de25ef76ebfe49434ab6a3757a2f24be208
Felipe@MBP2 <auditor400@gmail.com>
2019-11-16 21:54:25 +0530

    load, save, api

--- ui/libshape.js
+++ ui/libshape.js
@@ -8,21 +8,36 @@
 // 	"db": DB Shaped thing
 // 	""
 function getShape(points){
-
+	
+	// determine square:
+	// minpoints x,y
+	// maxpoints x,y
+	// draw square
 }
 
 // API Request: stores data from network onto local variable net
 function loadFromAPI(networkName){
 	//fetch api
+	if(!localStorage){
+		alert("Unsupported browser");
+		return {};
+	}
+	var net = localStorage.getItem(networkName) ? JSON.parse(localStorage.getItem(networkName)) : {};
+	return net;
 }
 
 //call API to render
 function saveToAPI(networkName, net){
+	if(!localStorage){
+		prompt("Unsupported browser, copy and paste please:",JSON.Stringify(net));
+		return;
+	}
 	// Save
-	
+	localStorage.setItem(networkName, JSON.Stringify(net));
 }
 //Do Render 
 function doRender(networkName, net){
+	// TODO: Include neeraj's code
 	alert("Rendering:" + networkName+"::" + JSON.Stringify(net));
 }
 var globalNetworkName="sample";
@@ -34,10 +49,13 @@ function getNetwork(){
 	return globalNetwork;
 }
 function getMenuOptions(){
-	return ("<div>"+
-		"<a href='#' onclick='loadFromAPI(prompt(\"Network Name:\"))' > Load</a>&nbsp;"+
-		"<a href='#' onclick='loadFromAPI(prompt(\"Network Name:\"))' > Save</a>&nbsp;"+
-		"<a href='#' onclick='SaveToAPI(prompt(\"Network Name:\"))' > Render</a>&nbsp;"+
-		"<a href='#' onclick='doRender(getNetworkName(),getNetwork())' > Load</a>&nbsp;"+
+	return ("<div class=palette>"+
+		"<a class='palette-item' href='#' onclick='loadFromAPI(prompt(\"Network Name:\"))' > Load</a>&nbsp;"+
+		"<a class='palette-item' href='#' onclick='loadFromAPI(prompt(\"Network Name:\"))' > Save</a>&nbsp;"+
+		"<a class='palette-item' href='#' onclick='SaveToAPI(prompt(\"Network Name:\"))' > Render</a>&nbsp;"+
+		"<a class='palette-item' href='#' onclick='doRender(getNetworkName(),getNetwork())' > Load</a>&nbsp;"+
 		"</div>")
+}
+function showButtonPalette(){
+	document.getElementById("toolbar").innerHTML = getMenuOptions();
 }
