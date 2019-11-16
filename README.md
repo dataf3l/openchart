c0a80d22c0eb583d605641b4bd6f0d82ad3bed59
Felipe@MBP2 <auditor400@gmail.com>
2019-11-17 03:18:49 +0530

    fix bug, add readme, add uploader

---
+++ README.md
@@ -0,0 +1,6 @@
+Find this on the cloud here:
+
+http://openchart.azurewebsites.net/ui/shape.html
+
+
+
---
+++ upload.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Uploads the website to the cloud.
+# https://docs.microsoft.com/en-us/azure/app-service/app-service-web-get-started-html
+
+az webapp up --location westeurope --name openchart
