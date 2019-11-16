ab833325966d05261d5dfdc86d2c220225292f78
Felipe@MBP2 <auditor400@gmail.com>
2019-11-16 21:13:50 +0530

    Property Editor

---
+++ ui/init.js
@@ -0,0 +1,6 @@
+// This is the main function
+function init(){
+	showButtonPalette();
+	//TEST:
+	showPropertyEditor("propertyEditor",0,{"type":"vm","x":"10","y":"10","width":"100","height":"100","name":"webserver","id":"0"});
+}
---
+++ ui/property_editor.js
@@ -0,0 +1,14 @@
+
+function propertyChanged(nodeId,propertyName,newValue){
+
+}
+
+// node: an object, i.e.
+// brew update && brew install azure-cli
+// {"type":"vm","x":"10","y":"10","width":"100","height":"100","name":"webserver","id":"0"}
+// if node has no properties, add an empty array.
+// for each property, show a text box
+// when the textboxcontent changes, call: propertyChanged()
+function showPropertyEditor(targetDivId, nodeId, node) {
+	
+}
