11ff464011e36901226dd4c8b40c855a42657369
Felipe@MBP2 <auditor400@gmail.com>
2019-11-17 06:41:42 +0530

    bezier

--- ui/line.js
+++ ui/line.js
@@ -1,9 +1,24 @@
 function line(ctx,x,y,x2,y2) {
 	ctx.beginPath();
 	ctx.moveTo(x, y);
+	ctx.bezierCurveTo( x2,y, x,y2, x2,y2 );
+	//ctx.lineTo(x2, y2);
+	ctx.strokeStyle = "#000000";
+	ctx.lineWidth = 2;
+	ctx.stroke();
+	ctx.closePath();
+}
+/*
+ *
+function line(ctx,x,y,x2,y2) {
+	ctx.beginPath();
+	ctx.moveTo(x, y);
 	ctx.lineTo(x2, y2);
 	ctx.strokeStyle = "#000000";
 	ctx.lineWidth = 2;
 	ctx.stroke();
 	ctx.closePath();
 }
+ *
+ *
+ * */
